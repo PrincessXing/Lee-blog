08cacbc05286f28ad0b7c5bf526dbd8fee62c7fe
JunFeng Liu <junfeng.liu@inc.run>
2023-10-27 16:19:04 +0800

    编写admin模块yml配置文件 修改中启动类mapper扫描注解

--- blog-framework/src/main/java/com/lychee/BlogFrameworkApplication.java
+++ blog-framework/src/main/java/com/lychee/BlogFrameworkApplication.java
@@ -1,11 +1,11 @@
 package com.lychee;
 
-import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Mapper
+@MapperScan("com.lychee.mapper")
 public class BlogFrameworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogFrameworkApplication.class,args);
